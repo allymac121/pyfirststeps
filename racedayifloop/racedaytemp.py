3ebf8c62a5ceca19dd17e4d04a809d41a0f8d262
allymac121 <54115472+allymac121@users.noreply.github.com>
2020-07-30 21:54:20 +0300

    Create racedaytemp.py

---
+++ racedayifloop/racedaytemp.py
@@ -0,0 +1,15 @@
+import random
+
+print("i see you brought the Mclaren today,feeling like racing are we?")
+print("you are...")
+
+temp = random.randint(1,8)
+if temp == 2:
+         print("good to race abby!")
+elif temp == 4:
+        print("maybe wait a little")
+elif temp == 7:
+        print("yeah better watch some track tapes,you are going nowhere")
+
+
+input("\n\nPress the enter key to exit.")
