081333090075e52b0dd35009e33ca8ed91a75739
allymac121 <54115472+allymac121@users.noreply.github.com>
2020-08-06 11:10:11 +0300

    Create shipping.py

---
+++ shippingcharges/shipping.py
@@ -0,0 +1,17 @@
+Ordertotal = 0
+shippingcost = 0
+orderwithshipping = 0
+
+Ordertotal = float(input("what is the order total for purchases made?"))
+
+if Ordertotal <= 100:
+    shippingcost == 0
+
+else:
+    shippingcost = 20
+
+orderwithshipping = Ordertotal + shippingcost
+
+print("your final total,including sjipping is: $ %.2f" %orderwithshipping )
+
+
