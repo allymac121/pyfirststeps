c67f7e6dd707abc9a9e607b41e493e3483abc4fc
allymac121 <54115472+allymac121@users.noreply.github.com>
2020-08-11 10:08:33 +0300

    Create deadlinecalcutor.py

---
+++ Deadlines/deadlinecalcutor.py
@@ -0,0 +1,34 @@
+import datetime
+
+#declare variables
+
+totalNoDays = 0
+strDeadline = ""
+NoWeeks = 0
+NoDays = 0
+
+#get the current date
+
+currentdate = datetime.date.today()
+
+#get the deadline date
+
+strDeadline = input("When is the task due (mm/dd/yyyy): ")
+
+deadline = datetime.datetime.strptime(strDeadline,"%m/%d/%Y").date()
+
+#get the number of days between the two
+
+totalNoDays = deadline - currentdate
+
+#get the weeks and days in between
+
+NoWeeks = totalNoDays.days / 7
+
+#number of days
+
+NoDays = totalNoDays.days % 7
+
+#show your results
+
+print(" you have %f weeks " %NoWeeks + "and %f days" %NoDays + " until the deadline")
